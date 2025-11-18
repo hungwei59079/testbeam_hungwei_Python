06c8bee11106c741d4ccb3753680686e44e90de7
hungwei <hungwei@LAPTOP-6VIAIBS1.localdomain>
2025-11-18 18:11:41 +0800

    debug

--- alignment/hit_analysis.py
+++ alignment/hit_analysis.py
@@ -69,17 +69,17 @@ n_pass = rdf_sel.Count().GetValue()
 print("Total events:", n_total)
 print("Passed selection:", n_pass)
 
-# coords_x = rdf_sel.Take[rdf.GetColumnType("WeightedX")]("WeightedX").GetValue()
-# coords_y = rdf_sel.Take[rdf.GetColumnType("WeightedY")]("WeightedY").GetValue()
+coords_x = rdf_sel.Take[rdf.GetColumnType("x_hits")]("WeightedX").GetValue()
+coords_y = rdf_sel.Take[rdf.GetColumnType("y_hits")]("WeightedY").GetValue()
 test_take = rdf_sel.Take[rdf.GetColumnType("HGCMetaData_trigTime")](
     "HGCMetaData_trigTime"
 ).GetValue()
 
 for i in range(5):
     print(f"Event {i}:")
-    print(f"TrigTime = {test_take}")
-    # for i in range(11):
-    # print(f"Layer {i}; X = {coords_x[i]}, Y = {coords_y[i]}")
+    # print(f"TrigTime = {test_take}")
+    for i in range(11):
+        print(f"Layer {i}; X = {coords_x[i]}, Y = {coords_y[i]}")
     print("-" * 30)
 
 """
