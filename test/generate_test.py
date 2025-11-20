41b3175963a9ec38da9f74774c4115d67b8a7658
Hung-Wei Liu <hungwei@lxplus999.cern.ch>
2025-11-20 09:48:46 +0100

    test/ is working fine

--- test/generate_test.py
+++ test/generate_test.py
@@ -1,3 +1,5 @@
+#Run root -l -b -q '../hexaplot_helper.C("test_values.txt", "test.png", true)' to generate test.png
+
 with open("test_values.py","w") as file:
     for i in range(222):
         file.write(f"{i}\n")
