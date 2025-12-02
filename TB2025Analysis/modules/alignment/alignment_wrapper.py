873a38f70d4780b718c3a99b8ca67354b70521a1
hungwei <hungwei@LAPTOP-6VIAIBS1.localdomain>
2025-12-02 21:39:56 +0800

    selection(run,outdir) completed. To be tested

--- TB2025Analysis/modules/alignment/alignment_wrapper.py
+++ TB2025Analysis/modules/alignment/alignment_wrapper.py
@@ -1,16 +1,17 @@
 import os
 
+from selection import selection
+
 
 def run(rdf, **kwargs):
-    NominalEnergies = kwargs.get("NominalEnergies")
-    areMC = kwargs.get("areMC")
     outdir = kwargs.get("outdir")
     outdir = os.path.normpath(outdir)
     outdir = os.path.abspath(outdir)
 
     print(outdir)
 
-    counts = rdf.Count().GetValue()
-    print(f"Number of events: {counts}")
+    # counts = rdf.Count().GetValue()
+    # print(f"Number of events: {counts}")
+    selection(rdf, outdir)
 
     return rdf
--- TB2025Analysis/modules/alignment/selection.py
+++ TB2025Analysis/modules/alignment/selection.py
@@ -3,100 +3,112 @@ import os
 
 import ROOT
 
-def selection(rdf):
-    
 
-# Step 1: Load the selection functions and declared objects from the helper script.
-ROOT.gInterpreter.ProcessLine(".L selection.C+")
+def selection(rdf, out_dir):
+    # Step 1: Load the selection functions and declared objects from the helper script.
+    ROOT.gInterpreter.ProcessLine(".L selection.C+")
 
-# Step 2: Load coordinates and initialize the uninitialize std::map
-with open("digi_coordinates.json") as f:
-    digi_coords = json.load(f)
+    # Step 2: Load coordinates and initialize the uninitialize std::map
+    with open("digi_coordinates.json") as f:
+        digi_coords = json.load(f)
 
-for ch, (x, y) in digi_coords.items():
-    ROOT.digiCoordMap[int(ch)] = ROOT.std.pair("float", "float")(x, y)
+    for ch, (x, y) in digi_coords.items():
+        ROOT.digiCoordMap[int(ch)] = ROOT.std.pair("float", "float")(x, y)
 
-rdf = ROOT.RDataFrame("Events", found_file_path).Define("entry", "rdfentry_")
+    n_total = rdf.Count().GetValue()
+    print("Total events:", n_total)
 
-# Comment the previous line and uncomment the following lines if you just want to test one file.
-# filename = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2025/SepTestBeam2025/Run112149/65ed5258-ab32-11f0-a4b8-04d9f5f94829/prompt/NANO_112149_999.root"
-# rdf = ROOT.RDataFrame("Events", filename).Define("entry", "rdfentry_")
+    print("Start event selection.")
 
-n_total = rdf.Count().GetValue()
-print("Total events:", n_total)
-
-print("Start event selection.")
-
+    """
+    rdf_sel = (
+        rdf.Filter(
+            "HGCMetaData_trigTime >= 18 && HGCMetaData_trigTime <= 21", "TrigTime selection"
+        )
+        .Filter("UniqueLayersCheck(HGCHit_layer)", "Unique layers >= 5")
+        .Filter("MaxHitsPerLayerCheck(HGCHit_layer)", "No layer with >= 4 hits")
+        .Filter(
+            "ArrayMatchCheck(HGCHit_layer, HGCDigi_channel)", "Layer/channel array match"
+        )
+        .Filter(
+            "AdjacentHitsCheck(HGCHit_layer, HGCDigi_channel)",
+            "Adjacent hit geometry check",
+        )
+        .Define("x_hits", "WeightedX(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
+        .Define("y_hits", "WeightedY(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
+    )
 """
-rdf_sel = (
-    rdf.Filter(
+
+    rdf_sel_1 = rdf.Filter(
         "HGCMetaData_trigTime >= 18 && HGCMetaData_trigTime <= 21", "TrigTime selection"
     )
-    .Filter("UniqueLayersCheck(HGCHit_layer)", "Unique layers >= 5")
-    .Filter("MaxHitsPerLayerCheck(HGCHit_layer)", "No layer with >= 4 hits")
-    .Filter(
+
+    n1_pass = rdf_sel_1.Count().GetValue()
+    print("Passed TrigTime selection:", n1_pass)
+
+    rdf_sel_2 = rdf_sel_1.Filter(
         "ArrayMatchCheck(HGCHit_layer, HGCDigi_channel)", "Layer/channel array match"
     )
-    .Filter(
-        "AdjacentHitsCheck(HGCHit_layer, HGCDigi_channel)",
-        "Adjacent hit geometry check",
+    n2_pass = rdf_sel_2.Count().GetValue()
+    print("Passed array match check:", n2_pass)
+
+    rdf_sel_3 = rdf_sel_2.Filter(
+        "UniqueLayersCheck(HGCHit_layer)", "Unique layers >= 5"
     )
-    .Define("x_hits", "WeightedX(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
-    .Define("y_hits", "WeightedY(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
-)
-"""
+    n3_pass = rdf_sel_3.Count().GetValue()
+    print("Passed unique layers check:", n3_pass)
 
-rdf_sel_1 = rdf.Filter(
-    "HGCMetaData_trigTime >= 18 && HGCMetaData_trigTime <= 21", "TrigTime selection"
-)
+    rdf_sel_4 = rdf_sel_3.Filter(
+        "MaxHitsPerLayerCheck(HGCHit_layer)", "No layer with >= 4 hits"
+    )
+    n4_pass = rdf_sel_4.Count().GetValue()
+    print("Passed hit<4 check:", n4_pass)
 
-n1_pass = rdf_sel_1.Count().GetValue()
-print("Passed TrigTime selection:", n1_pass)
+    rdf_sel_5 = (
+        rdf_sel_4.Filter("AdjacentHitsCheck(HGCHit_layer, HGCDigi_channel)")
+        .Define("x_hits", "WeightedX(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
+        .Define("y_hits", "WeightedY(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
+    )
 
+    n5_pass = rdf_sel_5.Count().GetValue()
+    print("Passed adjacent hit check:", n5_pass)
 
-rdf_sel_2 = rdf_sel_1.Filter(
-    "ArrayMatchCheck(HGCHit_layer, HGCDigi_channel)", "Layer/channel array match"
-)
+    out_file = os.path.join(out_dir, "selected_hits.root")
+    out_tree = "HitCoords"  # name of the output tree
 
-n2_pass = rdf_sel_2.Count().GetValue()
-print("Passed array match check:", n2_pass)
+    print(f"Selection complete. Saving selected coordinates to {out_file}...")
 
-rdf_sel_3 = rdf_sel_2.Filter("UniqueLayersCheck(HGCHit_layer)", "Unique layers >= 5")
+    cols_to_save = ROOT.std.vector("string")()
+    cols_to_save.push_back("x_hits")
+    cols_to_save.push_back("y_hits")
 
+    rdf_sel_5.Snapshot(out_tree, out_file, cols_to_save)
 
-n3_pass = rdf_sel_3.Count().GetValue()
-print("Passed unique layers check:", n3_pass)
-
-rdf_sel_4 = rdf_sel_3.Filter(
-    "MaxHitsPerLayerCheck(HGCHit_layer)", "No layer with >= 4 hits"
-)
-
-
-n4_pass = rdf_sel_4.Count().GetValue()
-print("Passed hit<4 check:", n4_pass)
-
-rdf_sel_5 = (
-    rdf_sel_4.Filter("AdjacentHitsCheck(HGCHit_layer, HGCDigi_channel)")
-    .Define("x_hits", "WeightedX(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
-    .Define("y_hits", "WeightedY(HGCHit_layer, HGCDigi_channel, HGCHit_energy)")
-)
+    print("Done. File saved.")
 
 
-n5_pass = rdf_sel_5.Count().GetValue()
-print("Passed adjacent hit check:", n5_pass)
+if __name__ == "__main__":
+    search_base = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2025/SepTestBeam2025/Run112149/65ed5258-ab32-11f0-a4b8-04d9f5f94829/prompt/"
+    os.makedirs("./selection_output", exist_ok=True)
+    outdir = os.path.join(os.getcwd(), "selection_output")
 
-out_file = "selected_hits.root"
-out_tree = "HitCoords"  # name of the output tree
+    found_file_path = []
+    for dirpath, _, filenames in os.walk(search_base):
+        for filename in filenames:
+            if "NANO" in filename and filename.endswith(".root"):
+                full_path = os.path.join(dirpath, filename)
+                found_file_path.append(full_path)
 
-print(f"Selection complete. Saving selected coordinates to {out_file}...")
+    print(f"found {len(found_file_path)} files. Processing......")
 
-cols_to_save = ROOT.std.vector("string")()
-cols_to_save.push_back("x_hits")
-cols_to_save.push_back("y_hits")
+    rdf = ROOT.RDataFrame("Events", found_file_path).Define("entry", "rdfentry_")
 
-rdf_sel_5.Snapshot(out_tree, out_file, cols_to_save)
+    # Comment the previous line and uncomment the following lines if you just want to test one file.
+    # filename = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2025/SepTestBeam2025/Run112149/65ed5258-ab32-11f0-a4b8-04d9f5f94829/prompt/NANO_112149_999.root"
+    # rdf = ROOT.RDataFrame("Events", filename).Define("entry", "rdfentry_")
 
-print("Done. File saved.")
+    selection(rdf, outdir)
+
 
 """
 event_index = rdf_sel.Take[rdf_sel.GetColumnType("entry")]("entry").GetValue()
